588f7a1ddf6b9425878494d5037d6d89743b7f36
ppayter-cc <pomsarpeter@gmail.com>
2016-05-26 09:15:30 +0200

    ready

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,8 +1,15 @@
 def palindrome(str):
-    return
+    # for i in range(len(str)):
+    str = str.replace(' ', '')
+    if str.lower() == str.lower()[::-1]:
+        return True
+    else:
+        return False
 
 
 def main():
+    str_input = input(str("Please enter a string! "))
+    print(palindrome(str_input))
     return
 
 
