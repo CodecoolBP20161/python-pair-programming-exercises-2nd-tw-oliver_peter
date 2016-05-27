3df980c8c80757db3256e0623bd50ea145b81a38
ppayter-cc <pomsarpeter@gmail.com>
2016-05-27 08:36:41 +0200

    finished the passwordgen

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,8 +1,24 @@
+import string
+import random
+
+
 def passwordgen():
-    return
+    lowercase = (list(string.ascii_lowercase))
+    uppercase = (list(string.ascii_uppercase))
+    digits = (list(string.digits))
+    punctuation = ('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?')
+    # actually if you use this instead, you can have more special characters,
+    # therefore a more secure password, but fail the test sometimes:
+    # punctuation = (list(string.punctuation))
+    charlist = ((random.sample(lowercase, 2)) + (random.sample(uppercase, 2)) +
+                (random.sample(digits, 2)) + (random.sample(punctuation, 2)))
+    random.shuffle(charlist)  # for better security :)
+    password = ''.join(charlist)
+    return password
 
 
 def main():
+    print(passwordgen())
     return
 
 
